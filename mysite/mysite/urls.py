769f0f8598eccbe61763f08cdf062e3d16ac1049
umut-dag <mlmoracle@yahoo.fr>
2021-08-29 23:59:45 +0200

    1ere partie du tutorial

---
+++ mysite/mysite/urls.py
@@ -0,0 +1,12 @@
+"""mysite URL Configuration"""
+
+from django.contrib import admin
+from django.urls import include, path
+""" Il faut toujours utiliser include() lorsque l’on veut inclure d’autres motifs d’URL.
+admin.site.urls est la seule exception à cette règle. """
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('polls/', include('polls.urls')),
+    path('', include('polls.urls')),
+]
